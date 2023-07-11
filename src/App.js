d554bf52779230fdc3daf3c6872d865f46e95aa2
unknown <sanchezlando@gmail.com>
2023-07-11 18:28:50 +0800

    Added Successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import MoviesPage from './Screens/MoviesPage';
+import AboutUs from './Screens/AboutUs';
+import ContactUs from './Screens/ContactUs';
+import Login from './Screens/Login';
+import Favorites from './Screens/Favorites';
+import NotFound from './Screens/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<HomeScreen/>}/>
+      <Route path='/movies' element={<MoviesPage/>}/>
+      <Route path='/about-us' element={<AboutUs/>}/>
+      <Route path='/contact-us' element={<ContactUs/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/favorites' element={<Favorites/>}/>
+      <Route path='*' element={<NotFound/>}/>
+
+    </Routes>
+      
   );
 }
 
-export default App;
+export default App
